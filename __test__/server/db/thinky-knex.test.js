d0bd385f4f0a16774a037130936d3739d784302f
Harpo Jaeger <harpo.jaeger@gmail.com>
2018-07-09 15:49:26 -0400

    update knex; getting closer to migrations working in the test environment

--- __test__/server/db/thinky-knex.test.js
+++ __test__/server/db/thinky-knex.test.js
@@ -3,10 +3,12 @@ const knex = require('knex')(NODE_ENV)
 import { r } from '../../../src/server/models'
 
 describe('The knexfile', () => {
-  it('returns the same interaction_step schema as the thinky adapter', async () => {
-    const vanillaKnexSchema = await knex('interaction_step').columnInfo()
-    const rKnexSchema = await r.knex('interaction_step').columnInfo()
-    console.log('vanilla', vanillaKnexSchema, 'r.knex', rKnexSchema)
+  beforeEach(async () => await knex.migrate.latest())
+  afterEach(async () => await knex.raw('DROP OWNED BY spoke_test;'))
+
+  it('provides an up-to-date interaction_step schema', async () => {
+    const interactionStepSchema = await knex('interaction_step').columnInfo()
+    console.log('interaction_step', interactionStepSchema)
     expect(1).toEqual(1)
   })
 })
